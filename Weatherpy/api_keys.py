c3b514994fc376d1fdd2e587242f6027da51b75e
KevinN <66852096+nguyenkevn@users.noreply.github.com>
2020-08-09 20:05:48 -0400

    Removing key

--- Weatherpy/api_keys.py
+++ Weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "fd602b1e4b78a4d14a27fe9c9961fc85"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyAfNjCBvwMOkUi_QdKRXcfqrFsKhwKRHug"
+g_key = ""
